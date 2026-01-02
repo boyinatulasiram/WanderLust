2f508dcc7cfc81b1ba7853dcbac9935426a1d8dd
Tulasi Ram Boyina <tulasiramboyina16@gmail.com>
2026-01-02 22:44:59 +0530

    commit now -m "Update listing features and fix bugs"

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
-const Listing = require("./models/listing.js")
-
+const Listing = require("./models/listing.js");
+const listing = require("./models/listing.js");
+const methodOverride = require("method-override");
 app.set("view enegine","ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -47,7 +50,7 @@ app.get("/listings", async(req,res) =>{
 })
 
 //new route
-app.get("/listings/new", async(req,res) =>{
+app.get("/listings/new", (req,res) =>{
     res.render("./listings/new.ejs")
 })
 
@@ -60,12 +63,30 @@ app.get("/listings/:id", async(req,res) =>{
 
 //create route
 app.post("/listings", async(req,res)=>{
-    let {title, description, image, price, location, country} = req.body.listing;
-    const newListing = new Listing({title,description,image,price,location,country});
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 })
+//edit route
+app.get("/listings/:id/edit",async(req,res) =>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
+});
 
+//update route
+app.put("/listings/:id",async(req,res) =>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+//delete route
+app.delete("/listings/:id",async(req,res) =>{
+    const {id} = req.params;
+    const delListing = await Listing.findByIdAndDelete(id);
+    console.log("Deleted Listing:",delListing);
+    res.redirect("/listings");
+})
 app.listen(8080,() =>{
     console.log("Server is running on port 8080");
 })
