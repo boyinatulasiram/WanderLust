477fba62eaf06a43352388bb49f0441f2dbb3ce4
Tulasi Ram Boyina <tulasiramboyina16@gmail.com>
2026-01-03 22:58:12 +0530

    Add edit form for listings in edit.ejs

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const path = require("path");
 const Listing = require("./models/listing.js");
 const listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 app.set("view enegine","ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
