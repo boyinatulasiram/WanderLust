7a88806f6ed1c3d128c276abda8dc2f4c8208e9a
Tulasi Ram Boyina <tulasiramboyina16@gmail.com>
2026-01-04 17:37:34 +0530

    jan3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-app.set("view enegine","ejs");
+app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
